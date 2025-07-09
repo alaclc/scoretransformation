fc808d60b83093ceb1ac4a6536f76aedd8f4b21c
Alexis Junior La Cruz <116528279+alaclc@users.noreply.github.com>
2025-07-08 22:34:05 -0300

    Delete Transformaciones Lineales Normas.R

--- Transformaciones Lineales Normas.R
+++
@@ -1,9 +0,0 @@
-tlineal <- function(variable,media,desviacion){
-media1 <- mean(variable)
-desviacion1 <- sd(variable)
-z <- (variable-media1)/desviacion1
-transformada <- media+(z*desviacion)
-ptransformado <- round(transformada,0)
-ptransformado <- data.frame(variable,ptransformado)
-print(ptransformado)
-}
